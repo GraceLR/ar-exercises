cc9f12149c3539aad61ddf035144d964fc66049b
GraceLR <gracerwork08@gmail.com>
2022-07-04 22:28:09 +0000

    add three stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store1 = Store.create(name: "store1", annual_revenue: 100, mens_apparel: 1, womens_apparel: 1)
+store2 = Store.create(name: "store2", annual_revenue: 200, mens_apparel: 1, womens_apparel: 1)
+store2 = Store.create(name: "store3", annual_revenue: 300, mens_apparel: 1, womens_apparel: 1)
+
