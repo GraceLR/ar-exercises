62333d5e1d24159858ffff69272538575fef704b
GraceLR <gracerwork08@gmail.com>
2022-07-04 22:31:07 +0000

    add required stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,7 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-store1 = Store.create(name: "store1", annual_revenue: 100, mens_apparel: 1, womens_apparel: 1)
-store2 = Store.create(name: "store2", annual_revenue: 200, mens_apparel: 1, womens_apparel: 1)
-store2 = Store.create(name: "store3", annual_revenue: 300, mens_apparel: 1, womens_apparel: 1)
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: 1, womens_apparel: 1)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: 0, womens_apparel: 1)
+store2 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: 1, womens_apparel: 0)
 
