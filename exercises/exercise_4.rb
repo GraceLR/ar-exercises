5c7bfb654bc2596be070262702471d902db35318
GraceLR <gracerwork08@gmail.com>
2022-07-05 18:20:12 +0000

    One-to-many association

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,9 +6,9 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
-store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: 1, womens_apparel: 0)
-store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
+Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: 1, womens_apparel: 0)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
 
 @mens_stores = Store.where(mens_apparel: 1)
 @mens_stores.each do |s|
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,9 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Ross", hourly_rate: 70)
+@store1.employees.create(first_name: "Sara", last_name: "Johnson", hourly_rate: 65)
+@store2.employees.create(first_name: "Sam", last_name: "Raj", hourly_rate: 60)
+@store2.employees.create(first_name: "Jie", last_name: "Chou", hourly_rate: 70)
+@store2.employees.create(first_name: "Josh", last_name: "Spears", hourly_rate: 55)
